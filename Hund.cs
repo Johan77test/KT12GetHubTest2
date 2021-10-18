bffc17ee9d7ad8f2456982cc92cd0f5e8262f248
Johan77test <92727672+Johan77test@users.noreply.github.com>
2021-10-18 15:12:38 +0200

    Lade till hundklassen

---
+++ Hund.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PingvinArv
+{
+    public class Pingvin
+    {
+        public static string artBeteckning = "A001";
+        public Pingvin(string forNamn, string efterNamn, double vikt)
+        {            
+            ForNamn = forNamn;
+            EfterNamn = efterNamn;
+            Vikt = vikt;
+        }
+
+        public string ForNamn { get; set; }
+        public string EfterNamn { get; set; }
+        public double Vikt { get; set; }
+        
+        public string ArtBet
+        {
+            get { return artBeteckning; }  //läser av objektets artbeteckning          
+        }
+    }
+}
