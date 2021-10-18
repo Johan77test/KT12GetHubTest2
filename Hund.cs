2e966d36fe49a3132bdc9eee22999ad5dc9639d1
Johan77test <92727672+Johan77test@users.noreply.github.com>
2021-10-18 15:21:13 +0200

    lade till id

--- Hund.cs
+++ Hund.cs
@@ -6,8 +6,10 @@ namespace PingvinArv
 {
     public class Hund
     {
+        private int id;
         public Hund(string forNamn, string efterNamn, double vikt)
-        {            
+        {
+            id = 1;
             ForNamn = forNamn;
             EfterNamn = efterNamn;
             Vikt = vikt;
