c5b97ff1f8cbd6115d18b1ae7c9353bd6f3cef67
Johan77test <92727672+Johan77test@users.noreply.github.com>
2021-10-21 10:13:40 +0200

    Andra ändringen

--- Hund.cs
+++ Hund.cs
@@ -7,11 +7,11 @@ namespace PingvinArv
     public class Hund
     {
         private int id;
-        public Hund(string forNamn, string efNamn, double vikt)
+        public Hund(string forNamn, string eftNamn, double vikt)
         {
             id = 4;
             ForNamn = forNamn;
-            EfterNamn = efNamn;
+            EfterNamn = eftNamn;
             Vikt = vikt;
         }
 
